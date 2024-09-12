1cf8f71530301474b1b93ec0dda95532ef3d1a97
Nikhil Patil <nikhilpatil222003@gmail.com>
2024-09-13 02:02:05 +0530

    file sync with docker container

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,11 @@ const app = express();
 const PORT = 3000;
 
 app.listen(PORT, ()=>{
-    console.log("Listening on port : ", PORT);
+    console.log("Listening on brosasasas : ", PORT);
 });
 
 app.get("/login",(req,res)=>{
-    console.log("Here you go!");
+    res.send("<p>bSHsdasdo</p>");
 });
 
 app.get("/register",(req,res)=>{
