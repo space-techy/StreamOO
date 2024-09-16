3a2aa81a274bdc5a8387f46ebc73b5c5d73afffb
Nikhil Patil <nikhilpatil222003@gmail.com>
2024-09-16 13:52:43 +0530

    added tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import Login from "./login";
-import Register from "./register";
+import Login from "./login";
+// import Register from "./register";
 
 function App() {
   return (
     <>
-      <Register />
-      {/* <Login /> */}
+      {/* <Register /> */}
+      <Login />
     </>
   )
 }
