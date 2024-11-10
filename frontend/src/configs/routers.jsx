b4e25339a6b63590a2672a81f229e5f29b95d166
Nikhil Patil <nikhilpatil222003@gmail.com>
2024-11-11 01:09:04 +0530

    change 3

--- frontend/src/configs/routers.jsx
+++ frontend/src/configs/routers.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/error";
 import ProtectedRoutes from "./protectedRoute";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import LogoutWay from "../pages/Log_out";
